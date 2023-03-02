6de55bd23aa102458fa743e3f683f77e45468f2c
Osamuyi <muyixone@yahoo.com>
2023-03-02 21:14:40 +0100

    Use http-auth to authenticate the registration route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const path = require('path');
+const auth = require('http-auth');
+const authConnect = require('http-auth-connect');
+
+const basic = auth.basic({
+  file: path.join(__dirname, '../users.htpasswd'),
+});
 
 const validator = require('../middleware/validationMiddleware');
 const { getIndexPage, postForm, getData } = require('../controller/index');
@@ -6,6 +13,6 @@ const { getIndexPage, postForm, getData } = require('../controller/index');
 const router = express.Router();
 
 router.get('', getIndexPage).post('', validator(), postForm);
-router.get('/registration', getData);
+router.get('/registration', authConnect(basic), getData);
 
 module.exports = router;
